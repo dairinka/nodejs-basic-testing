241f7a088ed200dc93b1371abf0f4c5feca43424
Iryna Yehorova <interest4me@gmail.com>
2024-02-05 21:33:05 +0200

    feat: add tests for task 8

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,25 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const list = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: null,
+          next: null,
+        },
+      },
+    };
+    const generateList = generateLinkedList([1, 2]);
+    expect(generateList).toStrictEqual(list);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const generateList = generateLinkedList([2, 3, 4]);
+    expect(generateList).toMatchSnapshot();
   });
 });
